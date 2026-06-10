8afa02614180c2dd0e590c28d11ff903959a4cc9
Khushi Rani <151911119+KhushiRani005@users.noreply.github.com>
2026-06-10 13:40:24 +0530

    Added new program

---
+++ even_no_rev.py
@@ -0,0 +1,10 @@
+#write a program using loop to print first 10 even numbers in reverse order.
+#1st way
+num=20
+while num>0:
+    print(num)
+    num= num-2
+  
+#2nd way
+for num in range(20,0,-3):
+    print(num)  
